2a589fc6b5c67d02802f2443ebcd336bfe12d44a
GargEngineers7002 <officialchiraggarg7002@gmail.com>
2026-06-23 03:27:45 +0530

    comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,59 @@
 #include "index.h"
 #include <iostream>
+#include <locale>
 #include <unistd.h>
 
+/* this ha
+ * s auto
+ * alignment because of the auto formatter
+ */
+/*hibyie
+ *hibyie intg
+ *hitn ilngsg
+ *htinds ling
+ *hitn oljefg
+ */
+/*
 int main()
 {
   int i = 8;
-  std::cout << x << std::endl;
+  std::cout << x << std::endl; // hi
   foo(i);
-  std::cout << x << std::endl;
-  std::cout << __cplusplus << std::endl;
-  return 0;
+  std::cout << x << std::endl;          // hi
+  std::cout << "hibyie i" << std::endl; // hi
+  /* hibyie
+   *
+   *
+   *
+   *
+   */
+return 0;
 }
+*/ < --this causes an error
+
+    /*
+    int main()
+    {
+      int i = 8;
+      std::cout << x << std::endl; // hi
+      foo(i);
+      std::cout << x << std::endl;          // hi
+      std::cout << "hibyie i" << std::endl; // hi
+      // hibyie
+      //
+      //
+      // hibyie
+      //
+
+    return 0;
+    }
+    */
+    < --this works
+
+          Preprocessor directive alternative:
+#if 0
+int main() {
+    /* Old multi-line comment */
+    int x = 5;
+}
+#endif
