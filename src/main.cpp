32dfc0c6d21f12dcdcfa751fcacbf0bedd8cb2d4
GargEngineers7002 <officialchiraggarg7002@gmail.com>
2026-06-25 06:51:45 +0530

    objects and variable

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,7 @@
 #include "index.h"
 #include <iostream>
-#include <locale>
 #include <unistd.h>
 
-/* this ha
- * s auto
- * alignment because of the auto formatter
- */
-/*hibyie
- *hibyie intg
- *hitn ilngsg
- *htinds ling
- *hitn oljefg
- */
-/*
 int main()
 {
   int i = 8;
@@ -21,39 +9,9 @@ int main()
   foo(i);
   std::cout << x << std::endl;          // hi
   std::cout << "hibyie i" << std::endl; // hi
-  /* hibyie
-   *
-   *
-   *
-   *
-   */
-return 0;
+  std::cout << 'h' << std::endl;
+  std::cout << -5.7 << std::endl;
+  std::cout << 5 << std::endl;
+  // variable is an object that has a name and a value
+  return 0;
 }
-*/ < --this causes an error
-
-    /*
-    int main()
-    {
-      int i = 8;
-      std::cout << x << std::endl; // hi
-      foo(i);
-      std::cout << x << std::endl;          // hi
-      std::cout << "hibyie i" << std::endl; // hi
-      // hibyie
-      //
-      //
-      // hibyie
-      //
-
-    return 0;
-    }
-    */
-    < --this works
-
-          Preprocessor directive alternative:
-#if 0
-int main() {
-    /* Old multi-line comment */
-    int x = 5;
-}
-#endif
