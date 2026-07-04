c678d6875fece2fb25f83616f29de692d444dd35
GargEngineers7002 <officialchiraggarg7002@gmail.com>
2026-07-04 16:28:47 +0530

    vcpkg

---
+++ note.md
@@ -0,0 +1,5 @@
+- so for reproducibility in vcpkg the baseline adding command is:
+
+```bash
+vcpkg x-update-baseline --add-initial-baseline
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,6 @@
 #include "index.h"
+#include <fmt/core.h>
 #include <iostream>
 #include <ostream>
 
-int main()
-{
-  std::cout << "Enter a number: ";
-
-  int i{};
-  std::cin >> i;
-
-  std::cout << "Enter another number: ";
-
-  int j{};
-  std::cin >> j;
-
-  std::cout << i << " + " << j << " is " << i + j << ".\n";
-  std::cout << i << " - " << j << " is " << i - j, std::cout << ".\n";
-}
+int main() { fmt::print("Hello, {}!\n", "World"); }
