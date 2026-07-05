d5d20b926477873a1572b12511bb4970afb3dec5
GargEngineers7002 <officialchiraggarg7002@gmail.com>
2026-07-05 20:03:56 +0530

    status codes or exit codes or return codes defined in the c++ standard

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include "index.h"
+#include <cstdlib>
 #include <fmt/core.h>
 #include <iostream>
 #include <ostream>
 
-void baz();
+int main()
+{
+  // success
+  return 0;
+  return EXIT_SUCCESS; // preprocessor macro defined in the cstdlib
 
-void bar() { baz(); }
-void baz() { bar(); }
-int main() { bar(); }
+  // fails
+  return 1;
+  return EXIT_FAILURE; // preprocessor macro defined in the cstdlib
+}
